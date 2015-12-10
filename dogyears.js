f6883f7ca53e31784503b3d0773e257a576b800c
wanda13 <wanda13@users.noreply.github.com>
2015-12-10 15:47:48 -0800

    Delete dogyears.js since created /dogyears

--- dogyears.js
+++
@@ -1,8 +0,0 @@
-function dogYears() {
-    var name = prompt("Enter your dog's name: ");
-    var age = prompt("Enter your dog's age: " );
-    var ageInDogYears = age * 7;
-    alert("Name of dog: " + name);
-    alert("Age in dog years: " + ageInDogYears);
-}
-dogYears();
